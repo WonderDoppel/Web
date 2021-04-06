9af1332ff193c7e185e317a090d479e72d327536
WonderDoppel <wondermonder.ch@gmail.com>
2021-04-06 19:15:10 -0400

    subir archivo

---
+++ visual studioC/ts/inicio.ts
@@ -0,0 +1,82 @@
+/*variables*/
+let formulario:any=document.getElementById("form");
+let mensaje:any=document.getElementById("mensaje");
+
+/*submit buttom*/
+formulario.addEventListener("submit",function(evento){
+    evento.preventDefault();
+    console.log("estoy entrando?");
+    let aux:number =0;
+    aux+= validarRut(formulario);
+    aux+= validarTelefono(formulario);
+
+    
+    if(aux ==0){
+        Mensaje(mensaje,formulario);
+    }
+
+});
+/*validaciones*/
+function validarRut(formulario:any){
+    let rut = formulario[2].value;
+    let numero = parseInt(rut.slice(0,8));
+    let digito = rut.slice(9).toUpperCase();
+    let flag = false;
+        /*si el rut es inferior a 10 digitos*/
+        if(rut.length<10){
+            alert("Rut incorrecto. deber tener el formato 12345678-9");
+            return -1; 
+        }
+
+        /*comprobar si los digitos del numero son solo numeros*/
+        if(isNaN(numero)==false){
+            flag=true;
+        }
+        /*comprobar el digito verificador*/
+        if(isNaN(parseInt(digito))==false){
+            flag=true;
+        }
+        else if(digito!="K"){
+            flag=false;
+        }
+
+        if(flag==false){
+        alert("Error, ingrese el RUT nuevamente.")
+        return -1;
+        }
+
+        if(flag){
+            return 0;
+        }
+}
+
+function validarTelefono(formulario){
+    let telefono:any = formulario[5].value;
+    console.log(telefono);
+    if(telefono.length<9){
+        alert("el numero debe tener 9 digitos");
+        return -1;
+    } 
+    if(telefono.length>9){
+        alert("el numero debe tener 9 digitos");
+        return -1;
+    }
+    else{
+        return 0;
+    }
+}
+
+/*funcion del mensaje */
+function Mensaje(mensaje:any,formulario:any){
+    mensaje.innerHTML = "hemos recibido sus datos, pronto nos estaremos comunicando con usted";
+    mensaje.style.display="block";
+    formulario.style.display="none";
+}
+function limpiarDatos(){
+    formulario[1].value="";
+    formulario[2].value="";
+    formulario[3].value="";
+    formulario[4].value="";
+    formulario[5].value="";   
+    document.documentElement.scrollTop = 0;
+}
